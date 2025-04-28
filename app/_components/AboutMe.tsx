6b42acb2c8d17284e2bed6943ad5fdd49ebad0f6
3osmann <issamkhabbouchi@gmail.com>
2025-04-28 14:15:33 +0100

    add

---
+++ app/_components/AboutMe.tsx
@@ -0,0 +1,96 @@
+'use client';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import React from 'react';
+
+gsap.registerPlugin(ScrollTrigger, useGSAP);
+
+const AboutMe = () => {
+    const container = React.useRef<HTMLDivElement>(null);
+
+    useGSAP(
+        () => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    id: 'about-me-in',
+                    trigger: container.current,
+                    start: 'top 70%',
+                    end: 'bottom bottom',
+                    scrub: 0.5,
+                },
+            });
+
+            tl.from('.slide-up-and-fade', {
+                y: 150,
+                opacity: 0,
+                stagger: 0.05,
+            });
+        },
+        { scope: container },
+    );
+
+    useGSAP(
+        () => {
+            const tl = gsap.timeline({
+                scrollTrigger: {
+                    id: 'about-me-out',
+                    trigger: container.current,
+                    start: 'bottom 50%',
+                    end: 'bottom 10%',
+                    scrub: 0.5,
+                },
+            });
+
+            tl.to('.slide-up-and-fade', {
+                y: -150,
+                opacity: 0,
+                stagger: 0.02,
+            });
+        },
+        { scope: container },
+    );
+
+    return (
+        <section className="pb-section" id="about-me">
+            <div className="container" ref={container}>
+                <h6 className="text-8xl md:text-6xl font-thin mb-20 slide-up-and-fade">
+                I believe in delivering user-centered solutions backed by robust and scalable backend systems.<br />
+                 I focus on creating intuitive experiences while ensuring high performance,
+                  security, and future-ready foundations for every project I build.
+                </h6>
+                <p className="pb-3 border-b text-muted-foreground slide-up-and-fade">
+                    This is me.
+                </p>
+
+                <div className="grid md:grid-cols-12 mt-9">
+                    <div className="md:col-span-5">
+                        <p className="text-5xl slide-up-and-fade">
+                            Hi, I&apos;m ISSAM.
+                        </p>
+                    </div>
+                    <div className="md:col-span-7">
+                        <div className="text-lg text-muted-foreground max-w-[450px]">
+                            <p className="slide-up-and-fade">
+                                I&apos;m a frontend web developer and backend web developer dedicated to
+                                turning ideas into creative solutions. I
+                                specialize in creating seamless and intuitive
+                                user experiences.
+                            </p>
+                            <p className="mt-3 slide-up-and-fade">
+                                My approach focuses on creating scalable,
+                                high-performing solutions tailored to both user
+                                needs and business objectives. By prioritizing
+                                performance, accessibility, and responsiveness,
+                                I strive to deliver experiences that not only
+                                engage users but also drive tangible results.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AboutMe;
