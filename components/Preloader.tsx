845af56e08366cd4ee4eeb072543a736a6764fda
3osmann <issamkhabbouchi@gmail.com>
2025-04-28 16:16:35 +0100

    Update Preloader.tsx

--- components/Preloader.tsx
+++ components/Preloader.tsx
@@ -59,7 +59,7 @@ const Preloader = () => {
                 <span className="inline-block translate-y-full">S</span>
                 <span className="inline-block translate-y-full">A</span>
                 <span className="inline-block translate-y-full">M</span>
-                <span className="inline-block translate-y-full">_</span>
+                <span className="inline-block translate-y-full">{ ' ' }</span>
                 <span className="inline-block translate-y-full">K</span>
                 <span className="inline-block translate-y-full">H</span>
                 <span className="inline-block translate-y-full">A</span>
