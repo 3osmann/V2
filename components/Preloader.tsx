09fdc11e9ee089e067ea63ae837c5aa603eb6ae7
3osmann <issamkhabbouchi@gmail.com>
2025-05-05 11:56:55 +0100

    Update Preloader.tsx

--- components/Preloader.tsx
+++ components/Preloader.tsx
@@ -58,7 +58,8 @@ const Preloader = () => {
                 <span className="inline-block translate-y-full">S</span>
                 <span className="inline-block translate-y-full">S</span>
                 <span className="inline-block translate-y-full">A</span>
-                <span className="inline-block translate-y-full" style={{ marginRight: '10px' }}>M</span>
+                <span className="inline-block translate-y-full" style={{ marginRight: '10px' }}>M</span>    
+                <span className="inline-block translate-y-full" style={{ width: '10px' }}></span>
                 <span className="inline-block translate-y-full">K</span>
                 <span className="inline-block translate-y-full">H</span>
                 <span className="inline-block translate-y-full">A</span>
